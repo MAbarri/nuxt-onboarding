2d64b30f37ee44de820e67dbf4194ea93ca64479
ahmadjebari <ahmadjebari@gmail.com>
2024-11-13 16:28:00 +0100

    onboarding

--- layouts/default/modals/SignIn.vue
+++ layouts/default/modals/SignIn.vue
@@ -7,71 +7,156 @@ const signInLoading = ref(false);
 const authStore = useAuthStore();
 const toast = useToast();
 
-const logInWith = async (provider: string) => {
+/* const displayError = () => {
+  toast.add({
+    id: '1',
+    title: 'error', // This sets the toast type to error
+    description: 'incorrect username or password'
+  });
+
+ 
+}  */
+/* 
+const email = ref('');
+const password = ref('') */
+
+/* const logInWith = async (provider: string) => {
   signInLoading.value = true;
   await signIn(provider, { callbackUrl: "/dashboard?signInCallback=true" });
   setTimeout(() => {
     signInLoading.value = false;
     authStore.toggleSignInModal();
-  }, 500);
+  }, 500); 
+};*/
+
+const username = ref('');
+const password = ref('');
+const errorMessage = ref('');
+const usernameError = ref(false);
+const passwordError = ref(false);
+
+// Simulate login logic for demonstration (replace with actual API call)
+const mockLogin = (username, password) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (username === 'correctUsername' && password === 'correctPassword') {
+        resolve(true);
+      } else {
+        reject('Incorrect username or password');
+      }
+    }, 2000);
+  });
+};
+
+const handleLogin = async () => {
+  // Reset errors
+  usernameError.value = !username.value;
+  passwordError.value = !password.value;
+  errorMessage.value = '';
+
+  // Check for empty fields
+  if (!username.value || !password.value) {
+    errorMessage.value = "Please fill out all fields.";
+    return;
+  }
+
+  // Attempt login if fields are filled
+  try {
+    await mockLogin(username.value, password.value);
+    errorMessage.value = ''; // Clear error if login is successful
+    // Proceed with login (e.g., redirect to another page)
+  } catch (error) {
+    errorMessage.value = error; // Show incorrect username/password error
+  }
 };
 </script>
 <template>
   <UModal v-model="authStore.isSignInModalOpen" closable>
-    <div class="p-4">
-      <div>
-        <div class="mx-auto flex items-center justify-center">
-          <SvgLogo class="h-16" aria-hidden="true" />
-        </div>
-        <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
-          <!-- //Trick to avoid initial focus on the Close button (see https://github.com/nuxt/ui/issues/734) -->
-          <button class="opacity-0 w-0 h-0" />
-          <UButton
-            color="indigo"
-            :initial-focus="null"
-            variant="ghost"
-            icon="i-heroicons-x-mark-20-solid"
-            class="-my-1"
-            @click="authStore.toggleSignInModal()"
+    <form
+    class="space-y-4 md:space-y-6 p-5"
+    @submit.prevent="
+      signIn('credentials', { username: username, password: password })
+    "
+  >
+    <div>
+      <label
+        for="username"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >username</label
+      >
+      <input
+        v-model="username"
+        type="username"
+        name="username"
+        id="username"
+        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="name@company.com"
+        :error="usernameError"
+        required
+      />
+    </div>
+    <div>
+      <label
+        for="password"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >Password</label
+      >
+      <input
+        v-model="password"
+        type="password"
+        name="password"
+        id="password"
+        placeholder="••••••••"
+        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        :error="usernameError"
+        required
+      />
+    </div>
+    <div class="flex items-center justify-between">
+      <div class="flex items-start">
+        <div class="flex items-center h-5">
+          <input
+            id="remember"
+            aria-describedby="remember"
+            type="checkbox"
+            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
           />
         </div>
-        <div class="mt-3 text-center sm:mt-5">
-          <h3 class="text-xl font-semibold leading-6 text-gray-900">Sign in</h3>
-          <div class="mt-2">
-            <p class="text-sm text-gray-500">
-              Choose the Login Provider that suits you
-            </p>
-          </div>
+        <div class="ml-3 text-sm">
+          <label for="remember" class="text-gray-500 dark:text-gray-300"
+            >Remember me</label
+          >
         </div>
       </div>
-      <div class="mt-5 sm:mt-6">
-        <UButton
-          :loading="signInLoading"
-          :disabled="signInLoading"
-          @click.prevent="logInWith('google')"
-          type="button"
-          block
-          color="indigo"
-          icon="i-mdi-google"
-          size="lg"
-          class="mb-2"
-        >
-          Sign in with Google
-        </UButton>
-        <UButton
-          :loading="signInLoading"
-          :disabled="signInLoading"
-          @click.prevent="logInWith('google')"
-          type="button"
-          block
-          color="indigo"
-          icon="material-symbols:login"
-          size="lg"
-          class="mb-2"
-        >
-          Sign in
-        </UButton>
-      </div>
+      <a
+        href="#"
+        class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+        >Forgot password?</a
+      >
     </div>
+    <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+    <button
+    @click="handleLogin" 
+
+      type="submit"
+      class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+    >
+      Sign in
+    </button>
+    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+      Don’t have an account yet?
+      <a
+        href="#"
+        class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+        >Sign up</a
+      >
+    </p>
+  </form>
   </UModal>
 </template>
+<style scoped>
+.error-message {
+  color: red;
+  margin-top: 8px;
+}
+</style>
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,54 +1,53 @@
-import { NuxtAuthHandler } from "#auth";
-import GoogleProvider from "next-auth/providers/google";
-import { getUserById } from "~/lib/user";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "~/lib/prisma";
-const config = useRuntimeConfig();
+// file: ~/server/api/auth/[...].ts
+import { NuxtAuthHandler } from '#auth'
+import GithubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+const runtimeConfig = useRuntimeConfig()
 
 export default NuxtAuthHandler({
-  secret: config.AuthSecret,
+  pages: {
+    // Change the default behavior to use `/login` as the path for the sign-in page
+    signIn: '/',
+  },
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
-    GoogleProvider.default({
-      clientId: config.GoogleClientId,
-      clientSecret: config.GoogleClientSecret,
+    GithubProvider.default({
+      clientId: runtimeConfig.public.GITHUB_CLIENT_ID,
+      clientSecret: runtimeConfig.GITHUB_CLIENT_SECRET,
+    }),
+    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+    CredentialsProvider.default({
+      name: 'Credentials',
+      authorize(credentials: any) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // NOTE: THE BELOW LOGIC IS NOT SAFE OR PROPER FOR AUTHENTICATION!
+
+        const user = {
+          username: 'test@email.com',
+          password: 'pass',
+        }
+
+        if (
+          credentials?.username === user.username &&
+          credentials?.password === user.password
+        ) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // eslint-disable-next-line no-console
+          console.error(
+            'Warning: Malicious login attempt registered, bad credentials provided'
+          )
+
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
     }),
   ],
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "jwt",
-  },
-  callbacks: {
-    async session({ token, session, user }) {
-      if (session.user) {
-        if (token.sub) {
-          session.user.id = token.sub;
-        }
-
-        if (token.email) {
-          session.user.email = token.email;
-        }
-
-        session.user.name = token.name;
-        session.user.image = token.picture;
-        session.user.stripeCustomerId = token.stripeCustomerId;
-      }
-
-      return session;
-    },
-
-    async jwt({ token }) {
-      if (!token.sub) return token;
-
-      const dbUser = await getUserById(token.sub);
-
-      if (!dbUser) return token;
-
-      token.name = dbUser.name;
-      token.email = dbUser.email;
-      token.picture = dbUser.image;
-      token.stripeCustomerId = dbUser.stripeCustomerId;
-      return token;
-    },
-  },
-});
+})
